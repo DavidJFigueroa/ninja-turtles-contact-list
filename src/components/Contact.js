d34d5da313544e31f663834606ff4ce4e43dbe2c
David Figueroa <122026800+DavidJFigueroa@users.noreply.github.com>
2023-02-15 16:45:03 +0100

    fixed delete function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Contact = (props) => {
   const [visible, setVisible] = React.useState(true);
   const removeElement = () => {
     setVisible((prev) => !prev);
   };
-
-  return (
-    <div className="friend-item">
-      <h3>{props.name}</h3>
-      <h4>{props.email}</h4>
-      <h4>{props.phone}</h4>
-      {visible && <button onClick={removeElement}>Delete</button>}
-    </div>
-  );
+  if (visible)
+    return (
+      <div className="friend-item">
+        <h3>{props.name}</h3>
+        <h4>{props.email}</h4>
+        <h4>{props.phone}</h4>
+        {visible && <button onClick={removeElement}>Delete</button>}
+      </div>
+    );
+  return null;
 };
 
 export default Contact;
